a4a8f4ca2700a7b845f44d1658b813ae3c0ecf08
cheshireoctopus <chandlermoisen@gmail.com>
2014-03-25 21:35:33 -0400

    Build get request; returns json object on server side - still need to return to client

--- main.rb
+++ main.rb
@@ -3,7 +3,21 @@ require 'sinatra/reloader'
 require 'mechanize'
 require 'json'
 
-get '/' do
-  'Hello world!'
+get '/:query' do
+  agent = Mechanize.new
+  page = agent.get('http://duckduckgo.com')
+  form = page.form('x')
+
+  form.q = params[:query].to_s
+  page = agent.submit(form)
+
+  results = {}
+  page.search('.links_main').each do |link|
+    results.store(link.at("a").inner_html.to_s.gsub("<b>","").gsub("</b>",""), link.at("a")[:href]).to_json
+  end
+
+  puts results
 end
+
+
 
